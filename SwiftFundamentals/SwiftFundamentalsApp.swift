a71b7a83ff3254ca5b37ed2e3b31e788c51fd82f
AndreFoulem <timmycasual@icloud.com>
2023-02-23 12:36:07 -0800

    Initial Commit

---
+++ SwiftFundamentals/SwiftFundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFundamentalsApp.swift
+//  SwiftFundamentals
+//
+//  Created by AndreMacBook on 2023-02-23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
